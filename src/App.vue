7aefd8c472edf506121b060263b7a2af50bfd01f
xuguojing-xgj <xugu@acss.com.cn>
2023-01-01 02:58:24 +0800

    vue 中的 createWebHashHistory 模式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
         <h1>Vue Router</h1>
+        <div>
+            <router-link to="/">Login</router-link>
+        </div>
+        <div>
+            <router-link to="/reg">Reg</router-link>
+        </div>
+        <hr>
         <!-- 导入注册完成之后 使用 router-view 路由出口 路由匹配到的组件将渲染到这里 -->
         <router-view></router-view>
     </div>
--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,18 @@ const routes: Array<RouteRecordRaw> = [
 // 返回一个 router 实例
 const router = createRouter({
     // 路由模式
+    // vue2 mode histroy vue3 createWebHistory() 
+    /**
+     * vue2 mode hash vue3 createWebHashHistory() 
+     * URL带 # 号 原理是 location.hash 去匹配的  
+     * hash 是 URL中 hash(#) 及后面的那部分, 常用作用锚点在页面内进行导航, 
+     * 改变 URL 中的 hash 部分不会引起页面刷新
+     *  通过 hashchange 事件监听URL 中的变化, 改变URL的方式只有这几种
+     *  1. 通过浏览器前进后退改变URL
+     *  2. 通过<a>标签改变URL
+     *  3. 通过window.location 改变 URL
+     */
+    // vue2 mode abstact vue3createMemoryHistory()
     history: createWebHashHistory(),
     // 路由信息
     routes,
